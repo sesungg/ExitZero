8ce8ee88253b6bb196198699ee248ebf23485456
sesungg <sesungg@gmail.com>
2024-02-21 16:56:01 +0900

    feat : 기본 설정

---
+++ src/main/java/exitzero_user/ExitZeroUserApplication.java
@@ -0,0 +1,13 @@
+package exitzero_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExitZeroUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExitZeroUserApplication.class, args);
+    }
+
+}
